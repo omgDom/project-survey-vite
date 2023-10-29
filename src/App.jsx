92fc732e52b2d896d375ebf0ef6221e31bce532a
omgDom <106763412+omgDom@users.noreply.github.com>
2023-10-29 16:49:59 +0100

    23.10.29

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import { MainForm } from "./components/MainForm";
+import './App.css';
+// import { Data } from "./dataTypes/Data";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return 
+  <>
+    <h1>Project Inspirational People!</h1>
+
+
+    <MainForm />
+  </>;
 };
